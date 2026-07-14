7b9d82313e757291a766904b972952b35c1af34c
Nwang chheten lama <chhetenlama86@gmail.com>
2026-07-15 00:11:42 +0545

    whois_features to find domain_age

---
+++ src/feature_extraction/whois_features.py
@@ -0,0 +1,24 @@
+import whois
+from datetime import datetime
+
+def extract_domain_age(domain):
+
+    try:
+        domain_info=whois.whois(domain)
+
+        creation_date=domain_info.creation_date
+
+        if isinstance(creation_date,list):
+            creation_date=creation_date[0]
+    
+        if creation_date is None:
+            return 0
+
+        today=datetime.now(creation_date.tzinfo)
+
+        age_days=(today-creation_date).days
+        return age_days
+
+    except Exception as e:
+        print("WHOIS Error:",e)
+        return 0
